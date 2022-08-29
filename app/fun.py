8bf6c1b068968022aed1bf8bdacffc94ac3020b2
HakimGh <hakimghaziali@gmail.com>
2022-08-29 20:34:06 +0430

    some change in auto ML

--- app/fun.py
+++ app/fun.py
@@ -1,23 +1,25 @@
-from pprint import pprint
-
-import sklearn.datasets
-import sklearn.metrics
-import autosklearn.regression
-import numpy as np
-
-X= np.random.rand(3,2)
-y = np.random.rand(3,2)
-
-
-automl = autosklearn.regression.AutoSklearnRegressor(
-    time_left_for_this_task=120,
-    per_run_time_limit=30,
-)
-automl.fit(X, y, dataset_name='diabetes')
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from autosklearn.regression import AutoSklearnRegressor
+from autosklearn.metrics import mean_absolute_error as auto_mean_absolute_error
+# load dataset
 
 
 
-
-def funn():
+def fun():
     
-    return automl.leaderboard()
+    url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/auto-insurance.csv'
+    dataframe = read_csv(url, header=None)
+    data = dataframe.values
+    data = data.astype('float32')
+    X, y = data[:, :-1], data[:, -1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+    model = AutoSklearnRegressor(time_left_for_this_task=5*60, per_run_time_limit=30, n_jobs=8)
+    model.fit(X_train, y_train)
+    print(model.sprint_statistics())
+    y_hat = model.predict(X_test)
+    
+    mae = mean_absolute_error(y_test, y_hat)
+
+    return mae
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .fun import funn
+from .fun import fun
 from .forms import CForm
 
 def home(request):
@@ -11,7 +11,7 @@ def home(request):
 
         print('hey')
         form = CForm()
-        print(funn())
+        print(fun())
 
 
     elif request.method == 'POST' :
